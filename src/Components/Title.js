30c0d149ff2e4369e42f9cb319e59d4df0d63be9
PiYU <priyankakhot978@gmail.com>
2020-09-01 22:43:25 +0530

    photowall commit

---
+++ src/Components/Title.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import ReactTooltip from 'react-tooltip';
+
+class Title extends Component
+{
+  render()
+    {
+      return (
+
+      <div style={{}}>
+        <div style={{ float: 'center' }}>
+          <button data-tip="Room skipped for the day"  place="bottom" data-type="success">514</button>
+          <ReactTooltip />
+        </div>
+       
+        <div style={{ clear: 'both' }}></div>
+        <div>
+          <h1>{this.props.title}</h1>
+        </div>
+      </div>
+    )
+    }
+  }
+
+  export default Title
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import Main from './Components/Main'
+import './Styles/Stylesheet.css'
+
+
+
+ReactDOM.render(<Main/>,document.getElementById('root'))
